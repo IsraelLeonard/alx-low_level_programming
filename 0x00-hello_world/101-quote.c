27142563dc203daff8b617f17884cd0ef6a5062a
IsraelLeonard <leonardisrael7@gmail.com>
2022-06-02 13:57:50 -0700

    A program that prints to the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - To print to standard error
+ * Description: A program that prints output to standard error
+ * Return: 1
+ */
+int main(void)
+{
+fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
+}
