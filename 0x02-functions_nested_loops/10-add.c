3c37cca4db9c0c34ee74bbbe42538b0648c2f5e5
IsraelLeonard <leonardisrael7@gmail.com>
2022-06-08 14:40:19 -0700

    A program that returns the summation of two integer: n & m

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - A program that adds two integers and returns the result
+ * @n: first digit for the summation
+ * @m: second digit for the summation
+ * Return: The result of yhe summation
+ */
+int add(int n, int m)
+{
+	int x;
+
+	x = n + m;
+	return (x);
+}
