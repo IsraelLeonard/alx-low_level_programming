1fc567d607ef53cfc9fb38946da51e85a3291ea0
IsraelLeonard <leonardisrael7@gmail.com>
2022-06-08 03:58:40 -0700

    tasks update

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _isalpha - Aprogrem ghat checks for alphabets
+ *
+ * @c: Is the variable ised for the check
+ *
+ * Return: it returns (0) if true and (1) if false
+ */
+
+int _isalpha(int c)
+{
+	int test;
+
+	test = _isalpha(c);
+	if (test == 0)
+	{
+		return (0);
+	}
+	if (test != 0)
+	{
+	return (1);
+	}
+}
---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_sign - A function that prints the sign of a number
+ *
+ * @n: serves as a code checker
+ *
+ * Return: based on result
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+	return (1);
+	}
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		return (0);
+	}
+}
+
+
