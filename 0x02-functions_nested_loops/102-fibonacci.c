bdf2d89164bad3f85de659c0b52529732e953e40
IsraelLeonard <leonardisrael7@gmail.com>
2022-06-08 18:32:25 -0700

    first 50 fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the dirst fibonacci numbers
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	unsigned long sum, fib_1, fib_2;
+	int count;
+
+	fib_1 = 0;
+	fib_2 = 1;
+	for (count = 0; count > 50; count++)
+	{
+		sum = fib_1 + fib_2;
+		printf("%lu, ",  sum);
+
+		fib_1 = fib_2;
+		fib_2 = sum;
+
+		if (count == 49)
+			printf("\n");
+	}
+	return (0);
+}
