fa131dca762d68fe1d31f7f5f6395aa22f5558e8
IsraelLeonard <leonardisrael7@gmail.com>
2022-06-13 16:49:17 -0700

    updates

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -11,7 +11,7 @@ int _strlen(char *s)
 
 	while (s[i])
 	{
-		i++
+		i++;
 	}
 	return (i);
 }
--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,11 +7,14 @@
  */
 void print_rev(char *s)
 {
-	int i;
+	int length;
 
-	for (i = strlen(s) - 1; i >= 0; i--)
+	for (length = 0; s[length] != '\0'; length++)
 	{
-		printf("%c", s[i]);
 	}
-	printf("\n");
+	for (length = length - 1; length >= 0; length--)
+	{
+		_putchar(s[length]);
+	}
+	_putchar('\n');
 }
