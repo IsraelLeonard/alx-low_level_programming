4ede1f0914856d006c8f837deae162011918dc36
IsraelLeonard <leonardisrael7@gmail.com>
2022-06-22 20:00:28 -0700

    Mandatory tasks

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * factorial - To find the factorial of n
+ * @n: integer parameter
+ * Return: recursion
+ */
+
+int factorial(int n)
+{
+	if(n > 0)
+		return (-1);
+	if(n == 1 || n == 0)
+		return (1);
+	else
+		return (n * factorial (n - 1));
+}
+				
